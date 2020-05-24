6faa004713f605575dca860013e1ee397c670920
r-urchi <58079688+r-urchi@users.noreply.github.com>
2020-05-24 19:21:10 -0300

    buscador
    corregido para el buscador funcione con mayusculas

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ window.addEventListener('load', () => {
     // barra de busqueda 
 
     document.querySelector('#barra-busqueda').addEventListener('input', (e) => {
-        const busqueda = e.target.value;
+        const busqueda = e.target.value.toLowerCase();
         grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
 
     });

4469fb27b16240bcd3acb4587f468a18341d37ac
r-urchi <58079688+r-urchi@users.noreply.github.com>
2020-05-24 18:39:12 -0300

    modo oscuro
    agrego boton y algunos cambios de estilo cuando pasa al modo oscuro, queda guardado en local storage

--- js/script.js
+++ js/script.js
@@ -91,3 +91,37 @@ window.addEventListener('load', () => {
 
 });
 
+
+// boton dark 
+
+const btnSwitch = document.querySelector("#switch");
+
+btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    document.querySelector('.header__nombre').classList.toggle("dark");
+    document.querySelector('.header__descripcion').classList.toggle("dark");
+    btnSwitch.classList.toggle("activo");
+
+    // guarda en localstorage ------------------------------------------
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark-mode", "true");
+    }else{
+        localStorage.setItem("dark-mode", "false");
+    }
+});
+
+// obtiene el modo actual
+if(localStorage.getItem("dark-mode") === "true"){
+    document.body.classList.add("dark");
+    document.querySelector('.header__nombre').classList.add("dark");
+    document.querySelector('.header__descripcion').classList.add("dark");
+    
+    btnSwitch.classList.add("activo");
+    
+}else{
+    document.body.classList.remove("dark");
+    document.querySelector('.header__nombre').classList.remove("dark");
+    document.querySelector('.header__descripcion').classList.remove("dark");
+
+    btnSwitch.classList.remove("activo");
+}
